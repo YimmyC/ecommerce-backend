1e964b0c90d396f2c54b167528517f84dd4e3dbf
YimmyC <jim.cassano1@gmail.com>
2023-02-06 15:35:06 -0600

    seeding working well enough for now

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,33 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    shirts: {
+      type: DataTypes.STRING,
+    },
+    shorts: {
+      type: DataTypes.STRING,
+    },
+    music: {
+      type: DataTypes.STRING,
+    },
+    hats: {
+      type: DataTypes.STRING,
+    },
+    shoes: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
